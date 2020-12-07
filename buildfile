5b2ca1d99122a4af03765e725d48e40de3118458
lyrahgames <lyrahgames@mailbox.org>
2020-12-07 03:34:47 +0100

    Add persistent build system and build2 package

--- buildfile
+++ buildfile
@@ -1,10 +1,12 @@
-cxx.std = latest
-using cxx
+./: tests/ manifest doc{*.md} legal{COPYING}
 
-hxx{*}: extension = hpp
-cxx{*}: extension = cpp
+./: lib{lyrahgames-pareto}: lyrahgames/hxx{**}
+{
+  cxx.export.poptions = "-I$out_root" "-I$src_root"
+}
+hxx{**}: install.subdirs = true
 
-cxx.poptions =+ "-I$src_base"
+tests/: install = false
 
 ./: exe{main}: cxx{main} hxx{gpp}
 ./: exe{kursawe_nsga2}: cxx{kursawe_nsga2} hxx{**}
---
+++ tests/viennet.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <vector>
+//
+#include <lyrahgames/chrono.hpp>
+#include <lyrahgames/gpp.hpp>
+#include <lyrahgames/pareto/gallery/viennet.hpp>
+#include <lyrahgames/pareto/naive.hpp>
+
+int main() {
+  using namespace std;
+  using namespace lyrahgames;
+  using namespace pareto;
+
+  using real = float;
+  using config_vector = array<real, 2>;
+  using object_vector = array<real, 3>;
+
+  mt19937 rng{random_device{}()};
+  vector<object_vector> pareto_front{};
+
+  const auto f = viennet<float>;
+  const auto box = aabb<config_vector>{{-3, -3}, {3, 3}};
+
+  const auto t = time(
+      [&] { pareto_front = monte_carlo_pareto_front(f, box, 10'000, rng); });
+  cout << "Computation took " << t << "s for " << pareto_front.size()
+       << " points on the pareto front."
+       << "\n";
+
+  fstream pareto_file{"pareto.dat", ios::out};
+  for (auto& p : pareto_front)
+    pareto_file << p[0] << '\t' << p[1] << '\t' << p[2] << '\n';
+  pareto_file << flush;
+
+  gpp plot{};
+  plot << "splot 'pareto.dat' u 1:2:3 w p lt rgb '#ff3333' pt 13\n";
+  string input;
+  getline(cin, input);
+}
