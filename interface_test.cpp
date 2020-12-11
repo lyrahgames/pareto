99d37e5f9d9581a7038ee3888c5552d112ac59f7
lyrahgames <lyrahgames@mailbox.org>
2020-12-11 15:29:52 +0100

    Tweak API test

--- interface_test.cpp
+++ interface_test.cpp
@@ -21,8 +21,9 @@ template <floating_point real>
 struct kursawe_problem {
   static constexpr size_t configurations = 3;
   static constexpr size_t objectives = 2;
-  static constexpr array<real, 2> box[] = {{-5, 5}, {-5, 5}, {-5, 5}};
-  void operator()(real* x, real* y) {
+  // static constexpr array<real, 2> box[] = {{-5, 5}, {-5, 5}, {-5, 5}};
+  static constexpr auto box(size_t i) { return array<real, 2>{-5, 5}; }
+  void operator()(const real* x, real* y) {
     using namespace std;
     y[0] = -10 * (exp(real(-0.2) * sqrt(x[0] * x[0] + x[1] * x[1])) +
                   exp(real(-0.2) * sqrt(x[1] * x[1] + x[2] * x[2])));
@@ -38,8 +39,9 @@ template <floating_point real>
 struct poloni2_problem {
   static constexpr size_t configurations = 2;
   static constexpr size_t objectives = 2;
-  static constexpr array<real, 2> box[] = {{-M_PI, M_PI}, {-M_PI, M_PI}};
-  void operator()(real* x, real* y) {
+  // static constexpr array<real, 2> box[] = {{-M_PI, M_PI}, {-M_PI, M_PI}};
+  auto box(size_t i) { return pair<real, real>{-M_PI, M_PI}; }
+  void operator()(const real* x, real* y) {
     using namespace std;
     const auto square = [](auto x) { return x * x; };
     constexpr real a1 = 0.5 * sin(1) - 2 * cos(1) + sin(2) - 1.5 * cos(2);
@@ -55,6 +57,28 @@ struct poloni2_problem {
 template <floating_point real>
 poloni2_problem<real> poloni2{};
 
+template <floating_point real>
+struct zdt3_problem {
+  static constexpr size_t configurations = 30;
+  static constexpr size_t objectives = 2;
+  auto box(size_t i) { return pair<real, real>{0, 1}; }
+  void operator()(const real* x, real* y) {
+    using namespace std;
+
+    real g = 0;
+    for (size_t i = 1; i < configurations; ++i) g += x[i];
+    g = 1 + 9 * g / 29;
+
+    real h = x[0] / g;
+    h = 1 - sqrt(h) - h * sin(10 * M_PI * x[0]);
+
+    y[0] = x[0];
+    y[1] = h * g;
+  }
+};
+template <floating_point real>
+zdt3_problem<real> zdt3{};
+
 template <typename vector>
 constexpr auto domination(const vector& x, const vector& y) {
   assert(size(x) == size(y));
@@ -69,7 +93,7 @@ template <typename pareto_problem, typename RNG>
 inline auto pareto_optimization(pareto_problem problem, size_t n, RNG&& rng) {
   using namespace std;
 
-  using real = remove_pointer_t<lyrahgames::meta::argument<pareto_problem, 0>>;
+  using real = remove_pointer_t<lyrahgames::meta::argument<pareto_problem, 1>>;
   using configurations_type = vector<real>;
   using objectives_type = vector<real>;
 
@@ -84,8 +108,8 @@ inline auto pareto_optimization(pareto_problem problem, size_t n, RNG&& rng) {
     // Get random argument vector in AABB.
     for (size_t k = 0; k < problem.configurations; ++k) {
       const auto random = distribution(rng);
-      x[k] = random * pareto_problem::box[k][1] +
-             (1 - random) * pareto_problem::box[k][0];
+      const auto [a, b] = problem.box(k);
+      x[k] = random * a + (1 - random) * b;
     }
 
     // Check if it is a non-dominated point.
@@ -124,7 +148,8 @@ int main() {
   mt19937 rng{random_device{}()};
 
   // auto pareto_front = pareto_optimization(kursawe<float>, 10'000'000, rng);
-  auto pareto_front = pareto_optimization(poloni2<float>, 100'000, rng);
+  // auto pareto_front = pareto_optimization(poloni2<float>, 100'000, rng);
+  auto pareto_front = pareto_optimization(zdt3<float>, 1'000'000'000, rng);
 
   fstream pareto_file{"pareto.dat", ios::out};
   for (auto& p : pareto_front) pareto_file << p[0] << '\t' << p[1] << '\n';
