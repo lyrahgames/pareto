f4db71e96412e77a839004c8a897b4eb4def19b7
lyrahgames <lyrahgames@mailbox.org>
2021-06-15 20:51:12 +0200

    Add all-in-one headers for library and gallery

---
+++ lyrahgames/pareto/gallery/gallery.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <lyrahgames/pareto/gallery/kursawe.hpp>
+#include <lyrahgames/pareto/gallery/pawellek.hpp>
+#include <lyrahgames/pareto/gallery/viennet.hpp>
+#include <lyrahgames/pareto/gallery/zdt3.hpp>
---
+++ lyrahgames/pareto/pareto.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <lyrahgames/pareto/version.hpp>
+
+// Optimizer
+#include <lyrahgames/pareto/naive.hpp>
+#include <lyrahgames/pareto/nsga2.hpp>
+
+// Frontiers
+#include <lyrahgames/pareto/frontier.hpp>
+#include <lyrahgames/pareto/frontier_cast.hpp>
